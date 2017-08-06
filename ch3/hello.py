5e8ae3c60dcfb4720314ed1321b4f37a6fe3cdc3
chenzhiyong <chenzhiyong47@163.com>
2017-08-06 03:30:09 +0000

    codes in 3.1.1

--- ch3/hello.py
+++ ch3/hello.py
@@ -1,6 +1,7 @@
 # init
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
+
 
 
 app = Flask(__name__)
@@ -9,12 +10,11 @@ manager =Manager(app)
 
 @app.route('/')
 def index():
-	return "<h1>Hello world!</h1>"
+	return render_template('index.html')
 
-
-@app.route('/User/<name>')
+@app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+	return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
