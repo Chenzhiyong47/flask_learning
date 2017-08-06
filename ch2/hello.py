9bae0b42fa64c770ed6758c28b44906dea0e3e6f
chenzhiyong <chenzhiyong47@163.com>
2017-08-06 03:09:29 +0000

    codes finished in ch2

--- ch2/hello.py
+++ ch2/hello.py
@@ -1,6 +1,10 @@
 # init
 from flask import Flask
+#from flask import make_response
+from flask import redirect
+from flask import abort
 from flask_script import Manager
+
 
 app = Flask(__name__)
 manager =Manager(app)
@@ -8,9 +12,19 @@ manager =Manager(app)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>>'
+	#response = make_response('<h1>This document carries a cookie!</h1>')
+	#response.set_cookie('answer', '42')
+	#return response
+	return redirect('https://www.baidu.com')
 
-@app.route('/user/<name>')
+@app.route('/user/<id>')
+def get_user(id):
+	user = load_user(id)
+	if not user:
+		abort(404)
+	return '<h1>Hello, %s!</h1>'% user.name
+
+@app.route('/User/<name>')
 def user(name):
 	return '<h1>Hello, %s!</h1>' % name
 
